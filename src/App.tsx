2bfb67d33985e1896fa8f33328a773b8b0066c3f
a-brown77 <a.brown7795@gmail.com>
2022-01-27 19:59:43 -0500

    UI functional

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,11 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { ArraysEx } from "./ArraysEx";
-import { Table } from "../src/Table/Table";
-import { TableRow } from "../src/Table/TableRow/TableRow";
 
 function App() {
   return (
     <div className="App">
-      <ArraysEx>
-        <Table data={pets}></Table>
-      </ArraysEx>
+      <ArraysEx></ArraysEx>
     </div>
   );
 }
--- src/ArraysEx.tsx
+++ src/ArraysEx.tsx
@@ -1,16 +1,13 @@
 import { cp } from "fs/promises";
 import { useState } from "react";
+import { getEffectiveTypeParameterDeclarations } from "typescript";
 import "./App.css";
 import { Pet } from "./Pet";
 
 export function ArraysEx() {
-  const [colors, setColor] = useState(["red", "yellow", "blue"]);
+  const [colors, setColors] = useState(["red", "yellow", "blue"]);
 
-  function addColor(color: string): void {
-    let colorsCopy: string[] = colors;
-    colorsCopy.push(color);
-    setColor(colorsCopy);
-  }
+  const [newColor, setNewColor] = useState("");
 
   const [pets, setPets] = useState<Pet[]>([
     { name: "Nova", type: "dog", id: 1 },
@@ -18,16 +15,54 @@ export function ArraysEx() {
     { name: "Presley", type: "cat", id: 3 },
   ]);
 
+  function addColor(color: string) {
+    setColors([...colors, color]);
+  }
+
+  function removePet(pet: Pet) {
+    setPets(pets.filter((p) => p.id !== pet.id));
+  }
+
   return (
     <div>
-      <ul>
-        {colors.map((color: string) => (
-          <li key={colors.indexOf(color)}>{color}</li>
+      <ol>
+        {colors.map((color, index) => (
+          <li key={index}>{color}</li>
         ))}
-      </ul>
+      </ol>
       <button onClick={() => addColor("green")}>Add Green</button>
       <button onClick={() => addColor("orange")}>Add Orange</button>
       <button onClick={() => addColor("purple")}>Add Purple</button>
+      <input
+        value={newColor}
+        onChange={(e) => setNewColor(e.target.value)}
+        type="Text"
+      ></input>
+      <button onClick={() => addColor(newColor)}>Add Color</button>
+      <table>
+        <thead>
+          <tr>
+            <th>Name: </th>
+            <th>Type: </th>
+            <th>Id: </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+        </tbody>
+        {pets.map((pet) => (
+          <tr key={pet.id}>
+            <td>{pet.name}</td>
+            <td>{pet.type}</td>
+            <td>{pet.id}</td>
+            <button onClick={() => removePet(pet)}>Delete</button>
+          </tr>
+        ))}
+      </table>
     </div>
   );
 }
